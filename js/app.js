e7d7d4f6bedbe9f1f37c9ab006f56f6f72968b66
knoxgon <volkan.guven@hotmail.se>
2019-10-28 18:52:39 +0100

    Toolbar options added as parameter instead of html

--- js/app.js
+++ js/app.js
@@ -3,11 +3,28 @@
  */
 const options = {
   modules: {
-    toolbar: '#toolbar'
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean']                                         // remove formatting button
+    ]
   },
-  placeholder: 'Compose an epic...',
   readOnly: false,
-  theme: 'snow'
+  theme: 'snow',
+  
 };
 /**
  * Quill Editor
