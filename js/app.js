349b4e6676a5de0cc58525a6a95f52fe9a5e9620
knoxgon <volkan.guven@hotmail.se>
2019-10-29 08:11:45 +0100

    Reordered the list of toolbars and removed extra fontsize option

--- js/app.js
+++ js/app.js
@@ -4,12 +4,11 @@
 const toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'],
   ['blockquote', 'code-block'],
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
   [{ 'script': 'sub'}, { 'script': 'super' }],
   [{ 'indent': '-1'}, { 'indent': '+1' }],
   [{ 'direction': 'rtl' }],
-  [{ 'size': ['small', false, 'large', 'huge'] }],
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
   [{ 'color': [] }, { 'background': [] }],
   [{ 'font': [] }],
   [{ 'align': [] }],
