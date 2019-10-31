8afdca35032af31485e12ec05e0c6ebc8171c5dd
Robingorski <robin.gorski@student.kyh.se>
2019-10-31 12:46:46 +0100

    Animation timer added

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ const navSlide = () => {
       if (link.style.animation){
           link.style.animation = ''
       } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
+          link.style.animation = `navLinkFade 0.5s ease backwards ${index / 7 + 0.2}s`;
       }
   });
   //burger animation
