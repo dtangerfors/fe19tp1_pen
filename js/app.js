ee046717fe81a6ec2519bff60656c9b8f9caa28a
Ismail Güven <ismailguven4200@gmail.com>
2019-11-04 11:57:16 +0100

    Added functionality for auto-save

--- js/app.js
+++ js/app.js
@@ -22,18 +22,30 @@ const options = {
   readOnly: false,
   theme: 'snow'
 };
+
+const settings = {
+	autoSave: true,
+	activeTheme: null
+};
+
+const localSettings = JSON.parse(localStorage.getItem("user-settings"));
+if(localSettings) {	
+	settings.autoSave = localSettings.autoSave;
+	settings.activeTheme = localSettings.activeTheme;
+}
+
 /**
  * Quill Editor
  */
 const editor = new Quill('#editor-code', options);
 
-/*
+// 
 editor.on('text-change', function(delta, source) {
-  if(auto_save) {
-    //saveDocument();
+  if(settings.autoSave) {
+	//   saveDocument();
   }
 });
-*/
+
 /*
 class Note{
   constructor(text){
@@ -60,7 +72,7 @@ if(content) {
   editor.setContents(JSON.parse(content));
 }
 */
-var navSideBut = document.getElementById("note-list"); //set items
+var navSideBut = document.getElementById("set-storage"); //set items
 function RemoveItem(e) {
   //Remove button function
   let clickIDRemove = e.target.getAttribute("delete-value");
@@ -106,63 +118,30 @@ function EditItem(e){
   saveEditText(windowContent);
   storeContent(editText);
 }
-function newContent(value) {
-  if (value ===null) {
-    return []
-  }else{
-    return value;
-  }
-
-}
-function titleNumb() {
-  return JSON.parse(localStorage.getItem("save-notes")).length+1
-}
 function makeAndStoreContent() {
-  const makeAndStoreContentLoad = JSON.parse(localStorage.getItem("save-notes"))
-  var newContentLoad = newContent(makeAndStoreContentLoad);
-  var numbTitle = titleNumb();
-  const loadID = Number(loadEditID());
-  var numb = 0;
-  for (let i = 0; i < newContentLoad.length; i++) {
-    if (newContentLoad[i].id === loadID) {
-      console.log(newContentLoad[i].id + " " + loadID) 
-      newContentLoad[i].content=editor.getContents();
-
-      numb++;
-    }
+  var makeAndStoreContentLoad = JSON.parse(localStorage.getItem("save-notes"));
+  const saveItem = {
+    content: editor.getContents(),
+    id: Date.now()
   }
-  if (numb!==0) {
-  }
-  else{
-    const saveItem = {
-      content: editor.getContents(),
-      id: Date.now()
-    }
-    newContentLoad.push(saveItem);
-    
-    var removeBtn = document.createElement("button");
-    var editBtn = document.createElement("button");
-    const h1 = document.createElement("h1");
-    h1.innerText = "title"+numbTitle; //text that tells which to delete or edit 
-    removeBtn.innerHTML = "delete";
-    editBtn.innerHTML = "edit";
-    var attributeRemoveID = document.createAttribute("delete-value");
-    var attributeEditID = document.createAttribute("edit-value");
-    attributeRemoveID.value = saveItem.id;
-    attributeEditID.value = saveItem.id;
-    const listDiv = document.createElement("div");
-    console.log(attributeRemoveID)
-    removeBtn.setAttributeNode(attributeRemoveID);
-    editBtn.setAttributeNode(attributeEditID);
-    listDiv.append(h1);
-    navSideBut.append(listDiv);
-    h1.parentNode.insertBefore(removeBtn, h1.nextSibling);
-    removeBtn.parentNode.insertBefore(editBtn, removeBtn.nextSibling);
-    removeBtn.onclick = RemoveItem;
-    editBtn.onclick = EditItem;
-  }
-  storeContent(newContentLoad);
-  
+  makeAndStoreContentLoad.push(saveItem);
+  storeContent(makeAndStoreContentLoad);
+  var removeBtn = document.createElement("button");
+  var editBtn = document.createElement("button");
+  h1.innerText = saveItem.content.ops[0].insert; //text that tells which to delete or edit 
+  removeBtn.innerHTML = "delete";
+  editBtn.innerHTML = "edit";
+  var attributeRemoveID = document.createAttribute("delete-value");
+  var attributeEditID = document.createAttribute("edit-value");
+  attributeRemoveID.value = saveItem.id;
+  attributeEditID.value = saveItem.id;
+  console.log(attributeRemoveID)
+  removeBtn.setAttributeNode(attributeRemoveID);
+  editBtn.setAttributeNode(attributeEditID);
+  h1.parentNode.insertBefore(removeBtn, h1.nextSibling);
+  removeBtn.parentNode.insertBefore(editBtn, removeBtn.nextSibling);
+  removeBtn.onclick = RemoveItem;
+  editBtn.onclick = EditItem;
 }
 window.addEventListener("DOMContentLoaded", function () {
   renderItems();
@@ -170,11 +149,8 @@ window.addEventListener("DOMContentLoaded", function () {
 })
 function renderItems(){
   const renderList = JSON.parse(localStorage.getItem("save-notes"))
-  const newList  = newContent(renderList);
-  var title = 0;
-  for (let i = 0; i < newList.length; i++) {
-    title++;
-    var idNumb = newList[i].id;
+  for (let i = 0; i < renderList.length; i++) {
+    var idNumb= renderList[i].id;
     var removeBtn = document.createElement("button");
     var editBtn = document.createElement("button");
     var attributeRemoveID = document.createAttribute("delete-value");
@@ -182,7 +158,7 @@ function renderItems(){
     attributeRemoveID.value=idNumb;
     attributeEditID.value=idNumb;
     const h1 = document.createElement("h1");
-    h1.innerText = "title"+title;
+    h1.innerText = renderList[i].ops[0].insert;
     removeBtn.innerHTML = "delete";
     editBtn.innerHTML = "edit";
     var listDiv = document.createElement("div");
@@ -196,11 +172,7 @@ function renderItems(){
     editBtn.onclick = EditItem;
   }
 }
-
-//save button
-document.getElementById("save-btn").addEventListener("click", saveFunction);
-
-
+//save knappen
 function saveFunction() {
   makeAndStoreContent();
   clearContents();
@@ -221,7 +193,89 @@ function clearContents() {
 function storeContent(value) {
   localStorage.setItem("save-notes", JSON.stringify(value))
 }
+var navSideBut = document.getElementById("note-list");
 
+var loadVal=[];
+function loadNew(){
+  loadVal = localStorage.getItem("save-notes") ? JSON.parse(localStorage.getItem("save-notes")) : [];
+}
+function makeLi() {
+  for (let i = 0; i < loadVal.length; i++) {
+    console.log(i);
+    navSideBut.insertAdjacentHTML("beforeend", `<div><h1>text</h1><button class="delete-btn">X</button>
+  <button class="edit-btn">Edit</button></div>`)
+  }
+  //delete
+  var deletebtns; 
+  if (document.querySelectorAll(".delete-btn").length !== 0) {
+    deletebtns = document.querySelectorAll(".delete-btn"); //Alla Delete knappar 
+    for (let i = 0; i < deletebtns.length; i++) {
+      deletebtns[i].addEventListener("click", function () {
+        console.log("delete me!!");
+        loadVal.splice(i, 1);
+        saveTextValue();
+        deletebtns[i].parentNode.remove();
+
+        console.log(i);
+      })
+    }
+  }
+  if (document.querySelectorAll(".edit-btn").length!==0) {
+    var editBtn = document.querySelectorAll(".edit-btn");
+    for (let j = 0; j < editBtn.length; j++) {
+      editBtn[j].addEventListener("click", function () {
+        //editor
+        console.log("Edit" + loadVal[j]);
+        editor.setContents(loadVal[j]);
+      });
+    }
+  }
+    
+}
+
+//save button
+document.getElementById("save-btn").addEventListener("click",saveFunction);
+
+function saveFunction(){
+  var edidtorText = editor.getContents();
+  titleOfText="text";
+  loadVal.push(edidtorText);
+  console.log(loadVal);
+
+  navSideBut.insertAdjacentHTML("beforeend", `<div><h1>${titleOfText}</h1><button class="delete-btn">X</button><button class="edit-btn">Edit</button></div>`);
+  saveTextValue();
+  //Delete 
+  if (document.querySelectorAll(".edit-btn")!==0){
+    var editBtn = document.querySelectorAll(".edit-btn");
+    for (let j = 0; j < editBtn.length; j++) {
+      //editor
+      editBtn[j].addEventListener("click", function () {
+        console.log("Edit"+j);
+        editor.setContents(loadVal[j])
+      });
+    }
+  }
+  if (document.querySelectorAll(".delete-btn").length !== 0) {
+    var deletebtns = document.querySelectorAll(".delete-btn")
+    for (let i = 0; i < deletebtns.length; i++) {
+      deletebtns[i].addEventListener("click", function () {
+        console.log("delete me!!");
+        loadVal.splice(i, 1);
+        saveTextValue();
+        deletebtns[i].parentNode.remove();
+      });
+    }
+  }
+}
+
+function saveTextValue(){
+  localStorage.setItem("save-notes", JSON.stringify(loadVal));
+}
+
+window.addEventListener("DOMContentLoaded",function () {
+  loadNew();
+  makeLi();
+});
 
 
 // Navbar
@@ -249,3 +303,32 @@ const navSlide = () => {
 
 
 navSlide();
+
+/**
+ * Save Checkbox Element in the DOM
+ */
+const save_checkbox = document.querySelector("input[name=auto-save]");
+save_checkbox.checked = settings.autoSave
+
+save_checkbox.addEventListener( 'change', function() {
+	settings.autoSave = this.checked;
+	localStorage.setItem("user-settings", JSON.stringify(settings));
+});
+
+const settingsModal = document.getElementById("settings-modal");
+const settingsButton = document.getElementById("settings");
+const closeElement = document.querySelector(".close");
+
+settingsButton.addEventListener('click', function() {
+  settingsModal.style.display = "block";
+});
+
+closeElement.addEventListener('click', function() {
+	settingsModal.style.display = "none";
+});
+
+window.onclick = function(event) {
+  if (event.target == settingsModal) {
+	settingsModal.style.display = "none";	
+  }
+};
