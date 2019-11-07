eaa8b2f5b72f93155751488353c3e89dc3aa3a17
Ismail Güven <ismailguven4200@gmail.com>
2019-11-07 10:02:38 +0100

    Added modules for notes

---
+++ js/modules/notes/note-list.js
@@ -0,0 +1,52 @@
+/**
+ * A list to store our notes in
+ */
+const Notes = [];
+
+/**
+ * Add a note to the Notes list
+ * @param {Note} note - accepts a Note object
+ */
+export function addNote(note) {
+    //If the input note is not a Note object, we don't add it
+    if(typeof note !== "object") return;
+    if(note.length) return;
+    if(!note.title || !note.content) return;
+    Notes.push(note);
+}
+
+/**
+ * Remove a note from a given index
+ * @param {number} index 
+ */
+export function removeBasedOnIndex(index) {
+    if (Notes[index]) {
+        Notes.splice(index, 1);
+    }
+}
+
+/**
+ * Removes the first note it finds with a given title
+ * @param {string} title 
+ */
+export function removeFirstFoundBasedOnTitle(title) {
+    const index = Notes.findIndex(t => t === title);
+    if(index) {
+        removeBasedOnIndex(index);
+    }
+}
+
+/**
+ * Retrieve a note based on a given index
+ * @param {number} index 
+ */
+export function getNote(index) {
+    return Notes[index]
+}
+
+/**
+ * Retrieve all notes
+ */
+export function getAllNotes() {
+    return Notes
+}
