cffe9701ca6320b9cf1c3d811821c63a23e96384
Mohmed-sal-ah <mohamed.salahahmed@student.kyh.se>
2019-11-12 11:13:24 +0100

    Merge conflict Resolved

--- js/modules/settings/quill-settings.js
+++ js/modules/settings/quill-settings.js
@@ -2,11 +2,8 @@ const toolbarOptions = [
     ['bold', 'italic', 'underline', 'strike'],
     ['blockquote', 'code-block'],
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    [{ 'script': 'sub'}, { 'script': 'super' }],
-    [{ 'indent': '-1'}, { 'indent': '+1' }],
-    [{ 'direction': 'rtl' }],
-    [{ 'align': [] }],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'indent': '-1' }, { 'indent': '+1' }],
     ['clean']
 ];
 
