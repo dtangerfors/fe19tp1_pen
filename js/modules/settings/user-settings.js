5d0a075e81ffcc9b30aa6637a454ecec128034cd
Ismail Güven <ismailguven4200@gmail.com>
2019-11-12 13:57:05 +0100

    Added functionality for changing theme

--- js/modules/settings/user-settings.js
+++ js/modules/settings/user-settings.js
@@ -3,7 +3,7 @@
  */
 export const settings = {
 	autoSave: false,
-	activeTheme: null
+	activeTheme: 'light'
 };
 
 /**
@@ -16,17 +16,49 @@ if(localSettings) {
 }
 
 export function saveUserSettings() {
-    localStorage.setItem('user-settings', JSON.stringify(settings));
+  localStorage.setItem('user-settings', JSON.stringify(settings));
 }
+
+/**
+ * 
+ * @param {String} theme 
+ */
+export function setTheme(theme) {
+  settings.activeTheme = theme;
+  document.documentElement.className = `theme-${settings.activeTheme}`;
+}
+
+/**
+ * 
+ */
+(function () {
+ if (settings.activeTheme === 'dark') {
+     setTheme('dark');
+ } else {
+     setTheme('light');
+ }
+})();
 
 /**
  * Save Checkbox Element in the DOM
  */
 const save_checkbox = document.querySelector('input[name=auto-save]');
-save_checkbox.checked = settings.autoSave
+save_checkbox.checked = settings.autoSave;
 
 save_checkbox.addEventListener('change', function () {
   settings.autoSave = this.checked;
+  saveUserSettings();
+});
+
+const theme_checkbox = document.querySelector('input[name=theme]');
+theme_checkbox.checked = settings.activeTheme === 'dark';
+
+theme_checkbox.addEventListener('change', function () {
+  if(this.checked) {
+    setTheme('dark');
+  }else {
+    setTheme('light');
+  }
   saveUserSettings();
 });
 
