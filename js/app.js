f0d01fc11359231d9d68e6955f8c90ee5f3baa30
Ismail Güven <ismailguven4200@gmail.com>
2019-11-14 11:36:41 +0100

    Added functionality for opening and closing the settings and notes buttons

--- js/app.js
+++ js/app.js
@@ -259,10 +259,23 @@ const navbarSlide = () => {
 
 function noteListSlide() {
   const note = document.getElementById('nav-note');
-  const noteList = document.querySelector('.sidebar');
+  const noteList = document.querySelector('#sidebar-notes');
+  const settings = document.getElementById('nav-settings');
+  const settingsList = document.querySelector('#sidebar-settings');
 
   note.addEventListener('click', function() {
+    if (settingsList.classList.contains('sidebar-show')) {
+      settingsList.classList.toggle('sidebar-show'); 
+    }
+    
+    noteList.classList.toggle('sidebar-show');
+  });
+  
+  settings.addEventListener('click', function() {
+    if (noteList.classList.contains('sidebar-show')){
       noteList.classList.toggle('sidebar-show');
+    }
+    settingsList.classList.toggle('sidebar-show');
   });
 }
 
