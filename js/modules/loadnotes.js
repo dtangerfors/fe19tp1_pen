0e9228a347b4ddbca2f049c4469a787f3bacc6af
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-11-15 11:38:17 +0100

    Added functions to control text length and date conversion

--- js/modules/loadnotes.js
+++ js/modules/loadnotes.js
@@ -14,22 +14,35 @@ function getTextFromContent(content) {
     return str;
 }
 
-function notesTemplate(note){
+function notesTemplate(note) {
+    // 360
+    let preview = getTextFromContent(note.content.ops);
+    if (preview.length > 350) {
+        preview = preview.substring(0, 347) + "..."
+    }
     return `
         <div class="notes">
             <h3 class="notes__title">${note.title}</h3>
-            <p class="notes__lastChanged">${note.lastChanged}</p>
-            <p class="notes__content">${getTextFromContent(note.content.ops)}</p>
+            <p class="notes__lastChanged">${convertDate(note.lastChanged)}</p>
+            <p class="notes__content">${preview}</p>
         </div>
     `
-
 }
 
-function displayNotes(note){
+function displayNotes(note) {
     return document.querySelector("#landing-page__note-list").innerHTML = note.map(notesTemplate).join("");
+}
+
+function convertDate(date) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let getYear = new Date(date).getFullYear()
+    let getMonth = new Date(date).getMonth()
+    let getDate = new Date(date).getDate()
+    return "Last edited " + months[getMonth] + " " + getDate + ", " + getYear
 }
 
 function main() {
     displayNotes(savedNotes);
 }
+
 window.addEventListener("DOMContentLoaded", main);
