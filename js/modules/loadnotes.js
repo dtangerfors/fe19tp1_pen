0737fce33710708bca226184028544687f3c03f9
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-11-19 15:04:18 +0100

    Removed dead code

--- js/modules/loadnotes.js
+++ js/modules/loadnotes.js
@@ -10,7 +10,7 @@ function notesTemplate(note) {
         preview = preview.substring(0, 347) + "..."
     }
     return `
-        <div class="notes">
+        <div class="notes" note-id="${note.dateOfCreation}">
             <h3 class="notes__title">${note.title}</h3>
             <p class="notes__lastChanged">Last edited ${dateHowLongAgo(note.lastChanged)}</p>
             <p class="notes__content">${preview}</p>
@@ -21,9 +21,3 @@ function notesTemplate(note) {
 export function displayNotes(note) {
     return document.querySelector("#landing-page__note-list").innerHTML = note.map(notesTemplate).join("");
 }
-
-export function sortNotesByLatestEdited(a, b) {
-    const dateA = new Date(a.lastChanged).getTime();
-    const dateB = new Date(b.lastChanged).getTime();
-    return dateA > dateB ? 1 : -1;
-}; 
