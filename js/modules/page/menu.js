44c7a743a1779f7b1f45cd2008619610eb2c8202
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-11-28 14:13:20 +0100

    Added function to toggle active classes on nav__link

--- js/modules/page/menu.js
+++ js/modules/page/menu.js
@@ -1,21 +1,26 @@
 export function noteListSlide() {
     const note = document.getElementById('nav-note');
     const noteList = document.querySelector('#sidebar-notes');
+    const navLinkNote = document.querySelector("#nav-link-note");
     const settings = document.getElementById('nav-settings');
     const settingsList = document.querySelector('#sidebar-settings');
+    const navLinkSettings = document.querySelector("#nav-link-settings");
 
     note.addEventListener('click', function () {
         if (settingsList.classList.contains('sidebar-show')) {
             settingsList.classList.toggle('sidebar-show');
+            navLinkSettings.classList.toggle('nav__link--active');
         }
-
         noteList.classList.toggle('sidebar-show');
+        navLinkNote.classList.toggle('nav__link--active');
     });
 
     settings.addEventListener('click', function () {
         if (noteList.classList.contains('sidebar-show')) {
             noteList.classList.toggle('sidebar-show');
+            navLinkNote.classList.toggle('nav__link--active');
         }
         settingsList.classList.toggle('sidebar-show');
+        navLinkSettings.classList.toggle('nav__link--active');
     });
 }
